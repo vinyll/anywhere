a9d335067deb5324d362c16415a2f50e52bc4927
Vincent Agnano <vincent.agnano@scopyleft.fr>
2015-04-26 10:54:04 +0200

    refactored code style

--- dist/anywhere.js
+++ dist/anywhere.js
@@ -2,11 +2,15 @@
   "use strict";
 
   var GithubExtractor = {
+    /**
+    Connect to rawgit.com, get the filename content and return it to
+    the callback.
+    */
     getContentFromFile: function(filename, callback) {
       var config = Anywhere;
       if(!config) {
-        throw new Error("You should declare an 'Anywhere' object with "
-              + "'user', 'repo' and 'branch' keys.");
+        throw new Error("You should declare an 'Anywhere' object with " +
+                        "'user', 'repo' and 'branch' keys.");
       }
       var url = "https://rawgit.com/{user}/{repo}/{branch}/{name}.md"
                 .replace('{user}', config.user)
@@ -20,8 +24,9 @@
         if (http.readyState === 4 && http.status === 200) {
           callback(http.responseText);
         } else if(http.status !== 200) {
-          throw new Error("'Anywhere' could not parse '{url}'."
-            "Please check your 'Anywhere' configuration.".replace('{url}', url));
+          throw new Error("'Anywhere' could not parse '{url}'." +
+                          "Please check your 'Anywhere' configuration."
+                          .replace('{url}', url));
         }
       }
       http.send();
@@ -30,14 +35,18 @@
 
   window.addEventListener('load', function(event) {
     var name = "", markedContent = "", nodeDisplay = "";
+
     [].forEach.call(document.querySelectorAll('[data-anywhere]'), function(node) {
       name = node.attributes['data-anywhere'].value;
+
       GithubExtractor.getContentFromFile(name, function(response) {
-        markedContent = marked(response);
         nodeDisplay = node.style.display || window.getComputedStyle(node).display;
+        markedContent = marked(response);
+
         if(nodeDisplay === 'inline') {
           markedContent = markedContent.substr(3, -4);
         }
+
         node.innerHTML = marked(markedContent);
       });
     });
